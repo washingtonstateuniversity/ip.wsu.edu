73559e88daf5fef4ec50ed08a1bc38a2b9f3c935
jeremyfelt <jeremy.felt@gmail.com>
2015-05-15 13:57:17 -0700

    Move fittext script into main theme JS file

--- functions.php
+++ functions.php
@@ -2,10 +2,7 @@
 
 
 function wpb_adding_scripts() {
-
 	wp_enqueue_script( 'wsu-ip-js', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery', spine_get_script_version(), true ) );
-wp_register_script('fittext', get_stylesheet_directory_uri() . '/js/jquery.fittext.js', array('jquery'), true);
-wp_enqueue_script('fittext');
 }
 
 add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
