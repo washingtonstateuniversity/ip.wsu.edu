1569b295a2a6e19e04f3772eff5277aa0ae2a79d
jeremyfelt <jeremy.felt@gmail.com>
2015-05-15 13:58:35 -0700

    Pedantic code style :)

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 
-
+add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
+/**
+ * Enqueue custom scripts for IP
+ */
 function wpb_adding_scripts() {
 	wp_enqueue_script( 'wsu-ip-js', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery', spine_get_script_version(), true ) );
 }
-
-add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
