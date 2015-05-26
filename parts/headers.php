8d31d4eb5a17206e5f925d14749163add5099ab7
jbickelhaupt <jbickelhaupt@wsu.edu>
2015-05-26 09:24:37 -0700

    Markup changes to headers.php
    Removed classes from div containing <sub> and changed <span> to <h1>
    (inside same div).

--- parts/headers.php
+++ parts/headers.php
@@ -25,9 +25,8 @@ if ( spine_get_option( 'main_header_show' ) == 'true' ) :
 		<sup class="sup-header" data-section="<?php echo $spine_main_header_values['section_title']; ?>" data-pagetitle="<?php echo $spine_main_header_values['page_title']; ?>" data-posttitle="<?php echo $spine_main_header_values['post_title']; ?>" data-default="<?php echo esc_html($spine_main_header_values['sup_header_default']); ?>" data-alternate="<?php echo esc_html($spine_main_header_values['sup_header_alternate']); ?>"><span class="sup-header-default"><?php echo strip_tags( $spine_main_header_values['sup_header_default'], '<a>' ); ?></span></sup>
 	</div>
 	
-	<div class="header-group hgroup guttered padded-bottom short">
+	<div class="header-group hgroup">
 		<sub class="sub-header" data-sitename="<?php echo $spine_main_header_values['site_name']; ?>" data-pagetitle="<?php echo $spine_main_header_values['page_title']; ?>" data-posttitle="<?php echo $spine_main_header_values['post_title']; ?>" data-default="<?php echo esc_html($spine_main_header_values['sub_header_default']); ?>" data-alternate="<?php echo esc_html($spine_main_header_values['sub_header_alternate']); ?>"><span class="sub-header-default"><?php echo strip_tags( $spine_main_header_values['sub_header_default'], '<a>' ); ?></span></sub>
-
 	</div>
 </header>
 
