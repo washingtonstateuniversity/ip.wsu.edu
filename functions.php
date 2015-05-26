dc7a8b71862aeaa94ca5302b246410dccacc2e5e
jeremyfelt <jeremy.felt@gmail.com>
2015-05-26 16:47:42 -0700

    Move to a class for the main theme functions for naming sanity

--- functions.php
+++ functions.php
@@ -1,25 +1,31 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
-/**
- * Enqueue custom scripts for IP
- */
-function wpb_adding_scripts() {
-	wp_enqueue_script( 'wsu-ip-js', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ), spine_get_script_version(), true );
-}
+class WSU_IP_Theme {
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_filter( 'wsu_color_palette_values', array( $this, 'color_palette_values' ) );
+	}
 
-add_filter( 'wsu_color_palette_values', 'wsu_ip_color_palette_values' );
-/**
- * Alter the defaults provided by the WSU Color Palette plugin.
- *
- * @return array
- */
-function wsu_ip_color_palette_values() {
-	return array(
-		'crimson' => array( 'name' => 'Crimson',  'hex' => '#981e32' ),
-		'blue1'   => array( 'name' => 'Blue One', 'hex' => '#4f868e' ),
-		'blue2'   => array( 'name' => 'Blue Two', 'hex' => '#2f5055' ),
-		'gray1'   => array( 'name' => 'Gray One', 'hex' => '#8d959a' ),
-		'gray2'   => array( 'name' => 'Gray Two', 'hex' => '#464e54' ),
-	);
+	/**
+	 * Enqueue custom scripts for International Programs.
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'wsu-ip-js', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ), spine_get_script_version(), true );
+	}
+
+	/**
+	 * Alter the defaults provided by the WSU Color Palette plugin.
+	 *
+	 * @return array
+	 */
+	public function color_palette_values() {
+		return array(
+			'crimson' => array( 'name' => 'Crimson',  'hex' => '#981e32' ),
+			'blue1'   => array( 'name' => 'Blue One', 'hex' => '#4f868e' ),
+			'blue2'   => array( 'name' => 'Blue Two', 'hex' => '#2f5055' ),
+			'gray1'   => array( 'name' => 'Gray One', 'hex' => '#8d959a' ),
+			'gray2'   => array( 'name' => 'Gray Two', 'hex' => '#464e54' ),
+		);
+	}
 }
+new WSU_IP_Theme();
