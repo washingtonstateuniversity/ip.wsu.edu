260d69b73e157177f62a265912961bc3bedb77f9
jeremyfelt <jeremy.felt@gmail.com>
2015-05-26 10:32:58 -0700

    Remove fittext for the time being

--- js/script.js
+++ js/script.js
@@ -1,44 +1,4 @@
-/*!
- * FitText.js 1.2
- *
- * Copyright 2011, Dave Rupert http://daverupert.com
- * Released under the WTFPL license
- * http://sam.zoy.org/wtfpl/
- *
- * Date: Thu May 05 14:23:00 2011 -0600
- */
-
 (function( $ ){
-
-	$.fn.fitText = function( kompressor, options ) {
-
-		// Setup options
-		var compressor = kompressor || 1,
-			settings = $.extend({
-				'minFontSize' : Number.NEGATIVE_INFINITY,
-				'maxFontSize' : Number.POSITIVE_INFINITY
-			}, options);
-
-		return this.each(function(){
-
-			// Store the object
-			var $this = $(this);
-
-			// Resizer() resizes items based on the object width divided by the compressor * 10
-			var resizer = function () {
-				$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-			};
-
-			// Call once to set.
-			resizer();
-
-			// Call on resize. Opera debounces their resize by default.
-			$(window).on('resize.fittext orientationchange.fittext', resizer);
-
-		});
-
-	};
-
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
@@ -51,10 +11,6 @@
 				return false;
 			}
 		}
-	});
-
-	$(function() {
-		$('span.sub-header-default').fitText(1.1, { minFontSize: '16px', maxFontSize: '84px' });
 	});
 
 	$(window).scroll(function() {
