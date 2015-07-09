2f8823b30f661ae1c1e228095bc60a037dbdac9e
jeremyfelt <jeremy.felt@gmail.com>
2015-07-09 13:48:05 -0700

    Move maps script enqueue into shorcode, account for multiple maps
    The "map" attribute will now work for more than one map.

--- includes/map-embed-shortcode.php
+++ includes/map-embed-shortcode.php
@@ -4,7 +4,6 @@ class WSUWP_IP_Map_Shortcode {
 	/**
 	 * Setup hooks
 	 */
-	public $loaded_map = 'ip-campus-map';
 	public function __construct() {
 		add_shortcode( 'wsu_ip_map', array( $this, 'display_map' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_map_script' ) );
@@ -22,7 +21,6 @@ class WSUWP_IP_Map_Shortcode {
 		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'wsu_ip_map' ) ) {
 			wp_enqueue_style( 'jquery-ui-smoothness', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.min.css', array(), false );
 			wp_enqueue_style( 'wsu-ip-map-style', 'https://beta.maps.wsu.edu/content/dis/css/map.view.styles.css', array(), false );
-			wp_enqueue_script( 'wsu-ip-map', 'https://beta.maps.wsu.edu/embed/'+$loaded_map, array( 'jquery' ), false, true );
 		}
 	}
 
@@ -40,7 +38,7 @@ class WSUWP_IP_Map_Shortcode {
 			'map' => '',
 		);
 		$atts = shortcode_atts( $default_atts, $atts );
-		$loaded_map=$atts['map'];
+
 		$map_path = sanitize_title_with_dashes( $atts['map'] );
 
 		if ( empty( $map_path ) ) {
@@ -49,6 +47,8 @@ class WSUWP_IP_Map_Shortcode {
 
 		$content = '<div id="map-embed-' . $map_path . '"></div>';
 		$content .= '<script>var map_view_scripts_block = true; var map_view_id = "map-embed-' . esc_js( $map_path ) .'";</script>';
+
+		wp_enqueue_script( 'wsu-ip-map', esc_url( 'https://beta.maps.wsu.edu/embed/ ' . $map_path ), array( 'jquery' ), false, true );
 
 		return $content;
 	}
