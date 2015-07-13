300d297d3c12647a9cb87ff5ef6f3907697a2e96
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-07-13 12:51:32 -0700

    Adds footer to site

---
+++ parts/footers.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Retrieve an array of values to be used in the header.
+ *
+ * site_name
+ * site_tagline
+ * page_title
+ * post_title
+ * section_title
+ * subsection_title
+ * posts_page_title
+ * sup_header_default
+ * sub_header_default
+ * sup_header_alternate
+ * sub_header_alternate
+ */
+$spine_main_header_values = spine_get_main_header();
+
+if ( spine_get_option( 'main_header_show' ) == 'true' ) :
+?>
+
+<footer class="main-header foot-title">
+	<div class="main-header-sitename">
+		<div class="header-group hgroup guttered padded-bottom short">
+			<sup class="sup-header" data-section="<?php echo $spine_main_header_values['section_title']; ?>" data-pagetitle="<?php echo $spine_main_header_values['page_title']; ?>" data-posttitle="<?php echo $spine_main_header_values['post_title']; ?>" data-default="<?php echo esc_html($spine_main_header_values['sup_header_default']); ?>" data-alternate="<?php echo esc_html($spine_main_header_values['sup_header_alternate']); ?>"><span class="sup-header-default"><?php echo strip_tags( $spine_main_header_values['sup_header_default'], '<a>' ); ?></span></sup>
+		</div>
+	</div>
+</footer>
+
+<?php endif; ?>
