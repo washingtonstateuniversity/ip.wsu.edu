077dad818d0e422344af2eb695af65d5264c73b7
jeremyfelt <jeremy.felt@gmail.com>
2015-08-10 21:39:01 -0700

    Account for extra classes on modal opening links, position X

--- includes/shortcode-form-modal.php
+++ includes/shortcode-form-modal.php
@@ -8,10 +8,12 @@ class IP_Form_Modal_Shortcode {
 	public function display_open_modal( $atts, $content ) {
 		$default_atts = array(
 			'id' => '',
+			'class' => '',
 		);
 		$atts = shortcode_atts( $default_atts, $atts );
 
-		$return_content = '<a href="#" class="trigger-modal" data-modal="modal-form-' . absint( $atts['id'] ) . '">';
+		$return_content = '<a href="#" class="trigger-modal ';
+		$return_content .= esc_attr( $atts['class'] ) . '" data-modal="modal-form-' . absint( $atts['id'] ) . '">';
 		$return_content .= esc_html( $content );
 		$return_content .= '</a>';
 
